b3e929fe9a6e701731ac62e70bba23f03814e100
Pavel-Sake <sake211091@gmail.com>
2019-10-09 22:42:35 +0300

    feature: add solution for 'morse-decoder' function

--- src/index.js
+++ src/index.js
@@ -38,9 +38,46 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let result = "";
+    let morseSymbols = "";
+    let counter = 0;
+    const arrayOfMorseSymbols = [];
+
+    const exprLength = expr.length;
+
+    for (let i = 0; i < exprLength; i += 2) {
+        const currentSymbol = expr[i];
+        const nextSymbol = expr[i + 1];
+        const currentCoupleSymbols = currentSymbol + nextSymbol;
+
+        if (currentCoupleSymbols === "10") {
+            morseSymbols += ".";
+        } else if (currentCoupleSymbols === "11") {
+            morseSymbols += "-";
+        }
+
+        if ((i + 2) % 10 === 0) {
+            arrayOfMorseSymbols[counter] = morseSymbols;
+            morseSymbols = "";
+            counter++;
+        }
+    }
+
+    const arrayOfMorseSymbolsLength = arrayOfMorseSymbols.length;
+
+    for (let i = 0; i < arrayOfMorseSymbolsLength; i++) {
+        const currentCodedCharacter = arrayOfMorseSymbols[i];
+
+        if(MORSE_TABLE[currentCodedCharacter] === undefined) {
+            result += " ";
+        } else {
+            result += MORSE_TABLE[currentCodedCharacter];
+        }
+    }
+
+    return result
 }
 
 module.exports = {
     decode
-}
+};
